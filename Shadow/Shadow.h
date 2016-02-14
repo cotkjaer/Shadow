591a370b737a5ebed394e233a665e6eafc00bc79
christian otkjær <christian.otkjaer@gmail.com>
2016-02-14 10:46:52 +0100

    Initial Commit

---
+++ Shadow/Shadow.h
@@ -0,0 +1,19 @@
+//
+//  Shadow.h
+//  Shadow
+//
+//  Created by Christian Otkjær on 14/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shadow.
+FOUNDATION_EXPORT double ShadowVersionNumber;
+
+//! Project version string for Shadow.
+FOUNDATION_EXPORT const unsigned char ShadowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shadow/PublicHeader.h>
+
+
